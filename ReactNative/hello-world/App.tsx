112ed26102cc5ec1edd2e36a2d80654e7d27edae
ArthurG-art <arthurggas2002@gmail.com>
2025-08-20 09:12:59 -0300

    .

---
+++ ReactNative/hello-world/App.tsx
@@ -0,0 +1,32 @@
+import { View,Text} from "react-native";
+
+export default function App (){
+  return(
+    <View style = {{justifyContent: 'center', alignItems: 'center'}}>
+      <Text style = {{justifyContent: 'center',}}>Auf thei hasneir </Text>
+    </View>
+
+  )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
